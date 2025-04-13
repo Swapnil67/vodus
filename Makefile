c0702e69d2aad40640ff261f2f8e08971840ab31
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-04-13 23:45:21 +0530

    Added giflib and created custom image format

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 PKGS=freetype2
-CXXFLAGS=-Wall -Wextra -Wunused-function -Wconversion -pedantic -ggdb -std=c++20 `pkg-config --cflags $(PKGS)`
-LIBS=`pkg-config --libs $(PKGS)` -lm
+CXXFLAGS=-Wall -Wextra -Wunused-function -Wconversion -pedantic -ggdb -std=c++20 -I/opt/homebrew/Cellar/giflib/5.2.2/include `pkg-config --cflags $(PKGS)` 
+GIFLIBS=-L/opt/homebrew/Cellar/giflib/5.2.2/lib -lgif
+LIBS=`pkg-config --libs $(PKGS)` $(GIFLIBS) -lm 
 
 vodus: main.cpp
 	g++ $(CXXFLAGS) -o vodus main.cpp $(LIBS)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,93 @@
 #include <cstdio>
 #include <cassert>
 #include <ft2build.h>
+#include <gif_lib.h>
 #include FT_FREETYPE_H
 
 #define FACE_FILE_PATH "./Comic-Sans-MS.ttf"
 
+struct Pixels32 {
+  uint8_t r, g, b, a;
+};
+
+// * Simple custom image fomat
+struct Image32 {
+  int height, width;
+  Pixels32 *pixels;
+};
+
+int save_image32_as_ppm(Image32 *image, const char* filename) {
+  FILE *f = fopen(filename, "wb");
+  if(!f) {
+    return -1;
+  }
+
+  // * Create a PPM file format
+  fprintf(f,
+          "P6\n"
+          "%d %d\n"
+          "255\n",
+          (int)image->width,
+          (int)image->height);
+
+  for (int row = 0; row < (int)image->height; ++row) {
+    for (int col = 0; col < (int)image->width; ++col) {
+      int index = ((int)image->width * row) + col;
+      Pixels32 p = *(image->pixels + index);
+      // printf("%c\n", x);
+      fputc(p.r, f);
+      fputc(p.g, f);
+      fputc(p.b, f);
+    }
+  }
+
+  fclose(f);
+  return 0;
+}
+
+// * Slap FreeType bitmap onto Image32
+void slap_onto_image32(Image32 *dest, FT_Bitmap *src, int x, int y) {
+  for (int row = 0;
+       (row < (int)src->rows) && (row + x < (int)dest->height);
+       ++row)
+  {
+    for (int col = 0;
+         (col < (int)src->width) && (col + y < (int)dest->width);
+         ++col)
+    {
+      int index = (row + y) * dest->width + col + x;
+      dest->pixels[index].r = src->buffer[row * src->pitch + col];
+      dest->pixels[index].g = src->buffer[row * src->pitch + col];
+      dest->pixels[index].b = src->buffer[row * src->pitch + col];
+    }
+  }
+}
+
+// * Slap giflib single image frame onto Image32
+void slap_onto_image32(Image32 *dest, SavedImage *src, ColorMapObject *SColorMap, int x, int y) {
+  assert(src);
+  assert(SColorMap);
+  assert(SColorMap->BitsPerPixel == 8);
+  assert(!SColorMap->SortFlag);
+  assert(src->ImageDesc.Top == 0);
+  assert(src->ImageDesc.Left == 0);
+
+  for (int row = 0;
+    (row < (int)src->ImageDesc.Height) && (row + x < (int)dest->height);
+    ++row) {
+    for (int col = 0;
+          (col < (int)src->ImageDesc.Width) && (col + y < (int)dest->width);
+          ++col) {
+      GifColorType pixel = SColorMap->Colors[src->RasterBits[row * src->ImageDesc.Width + col]];
+      dest->pixels[(row + y) * dest->width + col + x].r = pixel.Red;
+      dest->pixels[(row + y) * dest->width + col + x].g = pixel.Green;
+      dest->pixels[(row + y) * dest->width + col + x].b = pixel.Blue;
+    }
+  }
+}
+
+// * Save FreeType bitmap as a ppm file
+// * https://netpbm.sourceforge.net/doc/ppm.html [PPM Specification]
 int save_bitmap_as_ppm(FT_Bitmap *bitmap, const char* filename) {
   assert(bitmap->pixel_mode ==  FT_PIXEL_MODE_GRAY);
   FILE *f = fopen(filename, "wb");
@@ -12,6 +95,7 @@ int save_bitmap_as_ppm(FT_Bitmap *bitmap, const char* filename) {
     return -1;
   }
 
+  // * Create a PPM file format
   fprintf(f,
           "P6\n"
           "%d %d\n"
@@ -72,28 +156,35 @@ int main() {
   else if (error) {
     fprintf(stderr, "the %s could be opened and read.\n", FACE_FILE_PATH);
   }
-  
   printf("Loaded %s\n", FACE_FILE_PATH);
   printf("\tnum_glyphs = %ld\n", face->num_glyphs);
 
   error = FT_Set_Pixel_Sizes(
-    face,   /* handle to face object */
-    0,      /* pixel_width           */
-    64);   /* pixel_height          */
+      face, /* handle to face object */
+      0,    /* pixel_width           */
+      64);  /* pixel_height          */
 
-  if(error) {
+  if (error) {
     fprintf(stderr, "could not set font size in pixels\n");
     exit(1);
   }
+  FT_GlyphSlot slot = face->glyph;  /* a small shortcut */
 
-  FT_GlyphSlot  slot = face->glyph;  /* a small shortcut */
 
-  FT_Bitmap surface;
+  // * Read gif file
+  const char* gif_filepath = "cat-swag.gif";
+  GifFileType *gif_file = DGifOpenFileName(gif_filepath, &error);
+  if(error > 0) {
+    fprintf(stderr, "could not read gif file: %s\n", gif_filepath);
+  }
+  DGifSlurp(gif_file);
+
+  // * Custom Image32 Surface
+  Image32 surface;
   surface.width = 800;
-  surface.pitch = 800;
-  surface.rows = 600;
-  surface.buffer = (unsigned char *)malloc(surface.width * (unsigned int)surface.pitch);
-  surface.pixel_mode = FT_PIXEL_MODE_GRAY;
+  surface.height = 600;
+  surface.pixels = (Pixels32 *)malloc(surface.width * surface.height * sizeof(Pixels32));
+  assert(surface.pixels);
 
   const char *text = "Hello, World";
   size_t text_count = strlen(text);  
@@ -126,22 +217,21 @@ int main() {
     //   exit(1);
     // }
 
-    printf("\t'%c'\n", text[i]);
-    printf("\t\trows: %u\n", slot->bitmap.rows);
-    printf("\t\twidth: %u\n", slot->bitmap.width);
-    printf("\t\tpitch: %u\n", slot->bitmap.pitch);
-    printf("\t\tpixel_mode: %u\n", slot->bitmap.pixel_mode);
-
-    slap_bitmap_onto_bitmap(&surface,
-                            &face->glyph->bitmap,
-                            pen_x + slot->bitmap_left,
-                            pen_y - slot->bitmap_top);
+    slap_onto_image32(&surface,
+                      &face->glyph->bitmap,
+                      pen_x + slot->bitmap_left,
+                      pen_y - slot->bitmap_top);
 
     //* increment pen position
     pen_x += slot->advance.x >> 6;
 
-    save_bitmap_as_ppm(&surface, "output.ppm");
   }
+
+  slap_onto_image32(&surface, &gif_file->SavedImages[0], gif_file->SColorMap, 0, 0);
+
+  save_image32_as_ppm(&surface, "output.ppm");
+
+  DGifCloseFile(gif_file, &error);
   
   return 0;
 }
