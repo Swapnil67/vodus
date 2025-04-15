27af6cddac13abdacb17f7110b780ec94b1a814f
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-04-16 01:08:36 +0530

    integrate with libpng

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <ft2build.h>
 #include <gif_lib.h>
+#include <png.h>
 #include FT_FREETYPE_H
 
 #define FACE_FILE_PATH "./Comic-Sans-MS.ttf"
@@ -43,6 +44,21 @@ int save_image32_as_ppm(Image32 *image, const char* filename) {
 
   fclose(f);
   return 0;
+}
+
+// * Slap image32 onto Image32
+void slap_onto_image32(Image32 *dest, Image32 *src, int x, int y) {
+  for (int row = 0;
+       (row < (int)src->height) && (row + x < (int)dest->height);
+       ++row)
+  {
+    for (int col = 0;
+         (col < (int)src->width) && (col + y < (int)dest->width);
+         ++col)
+    {
+      dest->pixels[(row + y) * dest->width + col + x] = src->pixels[row * src->width + col];
+    }
+  }
 }
 
 // * Slap FreeType bitmap onto Image32
@@ -138,26 +154,65 @@ void slap_bitmap_onto_bitmap(FT_Bitmap *dest, FT_Bitmap *src, int x, int y)
   }
 }
 
-int main() {
+Image32 load_image32_from_png(const char *filepath) {
+  png_image png;
+  png.version = PNG_IMAGE_VERSION;
+  png.opaque = NULL;
+  
+  png_image_begin_read_from_file(&png, filepath);
+  if(png.warning_or_error) {
+    fprintf(stderr, "could not read file %s. %s\n", filepath, png.message);
+    exit(1);
+  }
+  
+  png.format = PNG_FORMAT_RGBA;
+  Pixels32 *buffer = (Pixels32 *)malloc(sizeof(Pixels32) * png.height * png.width);
+  
+  png_int_32 row_stride = 0;
+  png_image_finish_read(&png, NULL, buffer, row_stride, NULL);
+
+  free(buffer);
+  png_image_free(&png);
+
+  Image32 result = {
+      .width = (int)png.width,
+      .height = (int)png.height,
+      .pixels = buffer};
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  if(argc < 4) {
+    fprintf(stderr, "Usage: ./vodus <text> <gif_image> <png_image> [font]\n");
+    exit(1);
+  }
+
+  const char *text = argv[1];
+  const char *gif_filepath = argv[2];
+  const char *png_filepath = argv[3];
+  const char *font_face_file_path = FACE_FILE_PATH;
+  if (argc >= 5) {
+    font_face_file_path = argv[4];
+  }
+
   FT_Library  library;   /* handle to library     */
   FT_Face     face;      /* handle to face object */
-
   auto error = FT_Init_FreeType(&library);
+  error = FT_New_Face(library, font_face_file_path, 0, &face);
   if(error) {
     fprintf(stderr, "Could not initialize FreeType2\n");
     exit(1);
   }
 
-  error = FT_New_Face(library, FACE_FILE_PATH, 0, &face);
   if (error == FT_Err_Unknown_File_Format) {
-    fprintf(stderr, "the %s could be opened and read, but it appears that its font format is unsupported\n", FACE_FILE_PATH);
+    fprintf(stderr, "the %s could be opened and read, but it appears that its font format is unsupported\n", font_face_file_path);
     exit(1);
   }
   else if (error) {
-    fprintf(stderr, "the %s could be opened and read.\n", FACE_FILE_PATH);
+    fprintf(stderr, "the %s could be opened and read.\n", font_face_file_path);
   }
-  printf("Loaded %s\n", FACE_FILE_PATH);
-  printf("\tnum_glyphs = %ld\n", face->num_glyphs);
+  printf("Loaded %s\n", font_face_file_path);
+  // printf("\tnum_glyphs = %ld\n", face->num_glyphs);
 
   error = FT_Set_Pixel_Sizes(
       face, /* handle to face object */
@@ -172,7 +227,6 @@ int main() {
 
 
   // * Read gif file
-  const char* gif_filepath = "cat-swag.gif";
   GifFileType *gif_file = DGifOpenFileName(gif_filepath, &error);
   if(error > 0) {
     fprintf(stderr, "could not read gif file: %s\n", gif_filepath);
@@ -186,10 +240,9 @@ int main() {
   surface.pixels = (Pixels32 *)malloc((unsigned long)surface.width * (unsigned long)surface.height * sizeof(Pixels32));
   assert(surface.pixels);
 
-  const char *text = "Hello, World";
   size_t text_count = strlen(text);  
 
-  int pen_x = 0, pen_y = 100;
+  int pen_x = 0, pen_y = 50;
   for(int i = 0; i < (int) text_count; ++i) {
 
     // * retrieve glyph index from character code
@@ -227,7 +280,19 @@ int main() {
 
   }
 
-  slap_onto_image32(&surface, &gif_file->SavedImages[0], gif_file->SColorMap, 0, 0);
+
+  Image32 image32_png = load_image32_from_png(png_filepath);
+  slap_onto_image32(&surface,
+                    &image32_png,
+                    0, 100);
+
+  assert(gif_file->ImageCount > 0);
+  slap_onto_image32(&surface,
+                    &gif_file->SavedImages[0],
+                    gif_file->SColorMap,
+                    0, 200);
+
+
 
   save_image32_as_ppm(&surface, "output.ppm");
 
